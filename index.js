2943b9b54f46cde38c94988df9e830ac5f190071
Code-Algo <alexanderacollins@gmail.com>
2023-12-03 21:47:45 -0800

    added base url

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express')
 
 const app = express()
 
+const base = 'https://finance.yahoo.com'
+
 const url = 'https://finance.yahoo.com/etfs/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAANKBjYg0zM_n5aZCzgDGVj_qX06nBpyQ-r23c2OudoCBtIa46H9iB8dUpjK3H0VCIdT7_mQ2MdttS-NSAktVfXJ5pltzBMgOA0XQ5bVWIyK0Hf4rh4Trd8TWa2PiAGwC4YAJqvJdm8oM_y1yCMEmXlBrm53ZHqnH882nb_RmDu3W&offset=0&count=100'
 
 axios(url)
@@ -16,9 +18,10 @@ axios(url)
         $('[class*="simpTblRow Bgc($hoverBgColor):h"]').each(function () {
             const href = $(this).find('a').attr('href')
             const title = $(this).find('a').attr('title')
+
             etfs.push({
                 title,
-                href
+                href: base + href
             })
             console.log(etfs)
         })
